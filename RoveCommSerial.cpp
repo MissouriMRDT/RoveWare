4dae07129dab647b953db7baef69d9e5c0f3e2d4
Drue Satterfield <drscp5@mst.edu>
2017-10-20 03:11:23 -0500

    added crc error checking

--- RoveCommSerial.cpp
+++ RoveCommSerial.cpp
@@ -8,12 +8,14 @@
 #include "RoveBoard.h"
 #include <string.h>
 
-#define ROVECOMMSERIAL_HEADER_LENGTH 4
+#define ROVECOMMSERIAL_HEADER_LENGTH 6
 
 #define LENGTH_OFFSET       0
 #define FLAGS_OFFSET        1
 #define DATAID_OFFSET_MSB   2
 #define DATAID_OFFSET_LSB   3
+#define CRC_OFFSET_MSB      4
+#define CRC_OFFSET_LSB      5
 
 
 #define ROVECOMM_ACKNOWLEDGE_FLAG   1
@@ -26,6 +28,7 @@
 
 static void RoveCommParseMsg(uint8_t* buffer, uint16_t* dataID, uint8_t * size, void* data, uint8_t* flags);
 static void RoveCommHandleSystemMsg(RoveCommSerialHandle uartHandle, uint8_t* buffer, uint16_t* dataID, uint8_t* size, void* data, uint8_t* flags);
+static uint16_t crc16(const uint8_t* data_p, uint8_t length);
 
 RoveCommSerialHandle roveCommSerial_BeginSingle(uint16_t uart_index, uint16_t baud_rate, uint16_t txPin, uint16_t rxPin)
 {
@@ -82,33 +85,53 @@ bool roveComm_GetMsg(RoveCommSerialHandle uartHandle, uint16_t* dataID, uint8_t*
 
 static void RoveCommParseMsg(uint8_t* buffer, uint16_t* dataID, uint8_t * size, void* data, uint8_t* flags)
 {
+  uint16_t crcIn;
+  uint16_t crc;
   *dataID = (((uint16_t)(buffer[DATAID_OFFSET_MSB]) << 8) | buffer[DATAID_OFFSET_LSB] );
   *size = buffer[LENGTH_OFFSET];
   *flags = buffer[FLAGS_OFFSET];
+  crcIn = (((uint16_t)(buffer[CRC_OFFSET_MSB]) << 8) | buffer[CRC_OFFSET_LSB] );
 
   memcpy(data, &(buffer[ROVECOMMSERIAL_HEADER_LENGTH]), *size);
+
+  crc = crc16((uint8_t*)data, *size);
+
+  if(crc != crcIn)
+  {
+    *dataID = 0;
+    *size = 0;
+  }
 }
 
 bool roveComm_SendMsg(RoveCommSerialHandle uartHandle, uint16_t dataID, uint8_t size, const void* data)
 {
   uint8_t flags = 0;
+  uint16_t crc = 0;
   uint8_t packetSize = size + ROVECOMMSERIAL_HEADER_LENGTH;
   uint8_t buffer[packetSize];
+  crc = crc16((uint8_t*)data, size);
+
 
   buffer[LENGTH_OFFSET] = size;
   buffer[FLAGS_OFFSET] = flags;
   buffer[DATAID_OFFSET_MSB] = dataID >> 8;
   buffer[DATAID_OFFSET_LSB] = dataID & 0x00FF;
+  buffer[CRC_OFFSET_MSB] = crc >> 8;
+  buffer[CRC_OFFSET_LSB] = crc & 0x00FF;
 
   memcpy(&(buffer[ROVECOMMSERIAL_HEADER_LENGTH]), data, size);
 
   roveUartWrite(uartHandle, buffer, packetSize);
-
   return true;
 }
 
 static void RoveCommHandleSystemMsg(RoveCommSerialHandle uartHandle, uint8_t* buffer, uint16_t* dataID, uint8_t* size, void* data, uint8_t* flags)
 {
+  if(*dataID == 0)
+  {
+    return;
+  }
+
   if(*flags & ROVECOMM_ACKNOWLEDGE_FLAG != 0)
   {
     roveComm_SendMsg(uartHandle, ROVECOMM_ACKNOWLEDGE_MSG, sizeof(uint16_t), dataID);
@@ -150,3 +173,17 @@ uint16_t roveCommSerial_GetSerialBufferSize(RoveCommSerialHandle uartHandle)
   return roveUartGetBufferLength(uartHandle);
 }
 
+uint16_t crc16(const uint8_t* data_p, uint8_t length)
+{
+    uint8_t x;
+    uint16_t crc = 0xFFFF;
+
+    while (length > 0) //dont ask, from internet
+    {
+      length--;
+      x = crc >> 8 ^ *data_p++;
+      x ^= x>>4;
+      crc = (crc << 8) ^ ((unsigned short)(x << 12)) ^ ((unsigned short)(x <<5)) ^ ((unsigned short)x);
+    }
+    return crc;
+}
